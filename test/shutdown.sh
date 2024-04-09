adc2c82a96d64344198772929f470b43124a71a9
Your Name <your@email.com>
2024-04-09 09:17:22 +0200

    feat: shutdown now deletes both repos

--- test/shutdown.sh
+++ test/shutdown.sh
@@ -2,4 +2,7 @@
 
 # Delete GitHub repository
 curl -X DELETE -u "$GITHUB_USER:$GITHUB_TOKEN" \
-     https://api.github.com/repos/$GITHUB_USER/$REPO_NAME
+     https://api.github.com/repos/$GITHUB_USER/$REPO_ISSUES_NAME
+
+curl -X DELETE -u "$GITHUB_USER:$GITHUB_TOKEN" \
+     https://api.github.com/repos/$GITHUB_USER/$REPO_TEST_NAME
