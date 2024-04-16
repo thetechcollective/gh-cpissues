0fb4bcfe21735229a60572d68314e66287ced30b
Luchassmed <kapot@live.dk>
2024-04-16 13:41:56 +0000

    fixed #11

--- test/setup.sh
+++ test/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
+set -x
 #Assume that test.gitconfig is in the same folder as this script
 gh repo create $(git config --file test.gitconfig setup.dest-repo) --add-readme --description "Description of testrepo" --public
 
 pushd $(git rev-parse --show-toplevel)/
+gh repo clone $(gh api user --jq '.login')/$(git config --file ./test/test.gitconfig setup.dest-repo)
+pushd $(git config --file ./test/test.gitconfig setup.dest-repo)
+ln -s ../gh-cpissues gh-cpissues
 gh extension install .
+popd
 popd
