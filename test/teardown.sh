9995b2f241e58f1cef5f20f0a10995592cb84e2f
Luchassmed <kapot@live.dk>
2024-04-23 09:28:11 +0000

    if statement

--- test/teardown.sh
+++ test/teardown.sh
@@ -3,6 +3,11 @@ set -e -x
 
 #Assume that this is the correct root and that gitconfig is in test folder under root
 ROOT_DIR=/workspaces/gh-cpissues
+if [ -d "$ROOT_DIR" ]; then
+    ROOT_DIR=/workspaces/gh-cpissues
+else
+    ROOT_DIR=/home/runner/work/gh-cpissues/gh-cpissues
+fi
 USER=$(gh api user --jq '.login')
 
 path_to_gitconfig=$ROOT_DIR/test/test.gitconfig
