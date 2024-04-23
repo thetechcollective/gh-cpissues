cf8360b9ad3d7a809105c8e9d9fd3a2d98324161
Luchassmed <kapot@live.dk>
2024-04-23 08:10:47 +0000

    gitconfig

--- test/setup.sh
+++ test/setup.sh
@@ -7,8 +7,10 @@ USER=$(gh api user --jq '.login')
 
 path_to_gitconfig=$ROOT_DIR/test/test.gitconfig
 
-DEST_REPO=$(git config --file $path_to_gitconfig setup.dest-repo)
-README=$(git config --file $path_to_gitconfig setup.readme)
+#DEST_REPO=$(git config --file $path_to_gitconfig setup.dest-repo)
+#README=$(git config --file $path_to_gitconfig setup.readme)
+DEST_REPO="_testrepo"
+README="Desc of testrepo"
 
 gh repo create $DEST_REPO --add-readme --description "$README" --public
 
